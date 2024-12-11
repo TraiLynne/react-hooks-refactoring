07ec6647e7c3dfc34af91af4f402122e5c9d619b
Trai Lynne Compton <trailynne@gmail.com>
2024-12-11 15:47:42 -0700

    Complete `AnimationLogic`

--- app/src/components/examples/07_AnimationLogic/code.js
+++ app/src/components/examples/07_AnimationLogic/code.js
@@ -1,7 +1,43 @@
 export const beforeCode07 = `
-const BeforeComponent = () => <div>Before Component 1</div>;
+class AnimationClassComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			opacity: 1,
+		};
+		this.intervalId = null;
+	}
+
+	componentDidMount() {
+		this.intervalId = setInterval(() => {
+			this.setState((prevState) => ({
+				opacity: prevState.opacity === 1 ? 0.5 : 1,
+			}));
+		}, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.intervalId);
+	}
+
+	render() {
+		return <div style={{ opacity: this.state.opacity }}>Animating Text</div>;
+	}
+}
 `;
 
 export const refactoredCode07 = `
-const RefactoredComponent = () => <div>Refactored Component 1</div>;
+function AnimationComponent() {
+	const [opacity, setOpacity] = useState(1);
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setOpacity((prevOpacity) => (prevOpacity === 1 ? 0.5 : 1));
+		}, 1000);
+
+		return () => clearInterval(intervalId);
+	}, []);
+
+	return <div style={{ opacity }}>Animating Text</div>;
+}
 `;
--- app/src/components/examples/07_AnimationLogic/functional.js
+++ app/src/components/examples/07_AnimationLogic/functional.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
+
 function AnimationComponent() {
-	return (
-		<>
-			<h1>Hello World</h1>
-		</>
-	);
+	const [opacity, setOpacity] = useState(1);
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setOpacity((prevOpacity) => (prevOpacity === 1 ? 0.5 : 1));
+		}, 1000);
+
+		return () => clearInterval(intervalId);
+	}, []);
+
+	return <div style={{ opacity }}>Animating Text</div>;
 }
 
 export default AnimationComponent;
