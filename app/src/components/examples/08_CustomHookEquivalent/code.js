5430bcae06638292193a5f82ab38289c61c2abab
Trai Lynne Compton <trailynne@gmail.com>
2024-12-11 15:53:07 -0700

    Complete `CustomHook`

--- app/src/components/examples/08_CustomHookEquivalent/code.js
+++ app/src/components/examples/08_CustomHookEquivalent/code.js
@@ -1,7 +1,39 @@
 export const beforeCode08 = `
-const BeforeComponent = () => <div>Before Component 1</div>;
+class TimerClassComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			seconds: 0,
+		};
+	}
+
+	componentDidMount() {
+		this.timerId = setInterval(() => {
+			this.setState((prevState) => ({ seconds: prevState.seconds + 1 }));
+		}, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerId);
+	}
+
+	render() {
+		return <h1>Seconds: {this.state.seconds}</h1>;
+	}
+}
 `;
 
 export const refactoredCode08 = `
-const RefactoredComponent = () => <div>Refactored Component 1</div>;
+function TimerComponent() {
+	const [seconds, setSeconds] = useState(0);
+
+	useEffect(() => {
+		let timerId = setInterval(() => {
+			setSeconds(seconds + 1);
+		}, 1000);
+		return () => clearInterval(timerId);
+	});
+
+	return <h1>Seconds: {seconds}</h1>;
+}
 `;
--- app/src/components/examples/08_CustomHookEquivalent/functional.js
+++ app/src/components/examples/08_CustomHookEquivalent/functional.js
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
+
 function TimerComponent() {
-	return (
-		<>
-			<h1>Hello World</h1>
-		</>
-	);
+	const [seconds, setSeconds] = useState(0);
+
+	useEffect(() => {
+		let timerId = setInterval(() => {
+			setSeconds(seconds + 1);
+		}, 1000);
+		return () => clearInterval(timerId);
+	});
+
+	return <h1>Seconds: {seconds}</h1>;
 }
 
 export default TimerComponent;
