6ec390b27e2d1bff72d13e5fdb85a6057f1c452a
Trai Lynne Compton <trailynne@gmail.com>
2024-12-11 15:10:14 -0700

    Complete `Prop-Drilling`

--- app/src/components/examples/04_Prop-Drilling/code.js
+++ app/src/components/examples/04_Prop-Drilling/code.js
@@ -1,7 +1,41 @@
 export const beforeCode04 = `
-const BeforeComponent = () => <div>Before Component 1</div>;
+class ParentClassComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: "Hello from Parent",
+		};
+	}
+
+	render() {
+		return <ChildComponent message={this.state.message} />;
+	}
+}
+
+class ChildComponent extends Component {
+	render() {
+		return <GrandchildComponent message={this.props.message} />;
+	}
+}
+
+class GrandchildComponent extends Component {
+	render() {
+		return <p>{this.props.message}</p>;
+	}
+}
 `;
 
 export const refactoredCode04 = `
-const RefactoredComponent = () => <div>Refactored Component 1</div>;
+function ParentComponent() {
+	const [message, setMessage] = useState("Hello from Parent");
+	return <ChildComponent message={message} />;
+}
+
+function ChildComponent({ message }) {
+	return <GrandchildComponent message={message} />;
+}
+
+function GrandchildComponent({ message }) {
+	return <p>{message}</p>;
+}
 `;
--- app/src/components/examples/04_Prop-Drilling/functional.js
+++ app/src/components/examples/04_Prop-Drilling/functional.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 function ParentComponent() {
-	return (
-		<>
-			<h1>Hello World</h1>
-		</>
-	);
+	const [message, setMessage] = useState("Hello from Parent");
+	return <ChildComponent message={message} />;
+}
+
+function ChildComponent({ message }) {
+	return <GrandchildComponent message={message} />;
+}
+
+function GrandchildComponent({ message }) {
+	return <p>{message}</p>;
 }
 
 export default ParentComponent;
