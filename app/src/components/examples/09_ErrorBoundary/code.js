cfa42fe1c6510444f7db62fe29e69908d0c1e31e
Trai Lynne Compton <trailynne@gmail.com>
2024-12-11 15:57:23 -0700

    Complete `ErrorBoundary`

--- app/src/components/examples/09_ErrorBoundary/code.js
+++ app/src/components/examples/09_ErrorBoundary/code.js
@@ -1,7 +1,38 @@
 export const beforeCode09 = `
-const BeforeComponent = () => <div>Before Component 1</div>;
+class ErrorBoundaryClass extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { hasError: false };
+	}
+
+	componentDidCatch(error, info) {
+		console.error("Error caught:", error, info);
+		this.setState({ hasError: true });
+	}
+
+	render() {
+		if (this.state.hasError) {
+			return <h1>Something went wrong.</h1>;
+		}
+
+		return this.props.children;
+	}
+}
 `;
 
 export const refactoredCode09 = `
-const RefactoredComponent = () => <div>Refactored Component 1</div>;
+function ErrorBoundary({ children }) {
+	const [hasError, setHasError] = useState(false);
+
+	const errorHandler = (error, info) => {
+		console.error("Error caught:", error, info);
+		setHasError(true);
+	};
+
+	if (hasError) {
+		return <h1>Something went wrong.</h1>;
+	}
+
+	return children;
+}
 `;
--- app/src/components/examples/09_ErrorBoundary/functional.js
+++ app/src/components/examples/09_ErrorBoundary/functional.js
@@ -1,9 +1,18 @@
-function ErrorBoundary() {
-	return (
-		<>
-			<h1>Hello World</h1>
-		</>
-	);
+import { useState } from "react";
+
+function ErrorBoundary({ children }) {
+	const [hasError, setHasError] = useState(false);
+
+	const errorHandler = (error, info) => {
+		console.error("Error caught:", error, info);
+		setHasError(true);
+	};
+
+	if (hasError) {
+		return <h1>Something went wrong.</h1>;
+	}
+
+	return children;
 }
 
 export default ErrorBoundary;
